f97d687552bc41266104578d7fba83eef635fb06
fergmj <mindy@mindyferguson.com>
2016-03-11 09:12:40 -0800

    Fixed a POST method

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    post '/users/sign_out' => 'devise/sessions#destroy'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
